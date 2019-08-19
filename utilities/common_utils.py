0126afcd7ad23fa7715750659d7d89cdcccbdfee
Ramtin Yazdanian <ramtin.yazdanian@gmail.com>
2019-08-19 15:24:41 +0200

    permission error added

--- utilities/common_utils.py
+++ utilities/common_utils.py
@@ -6,7 +6,7 @@ def make_sure_path_exists(path):
     try:
         os.makedirs(path)
     except OSError as exception:
-        if exception.errno != errno.EEXIST:
+        if exception.errno != errno.EEXIST and exception.errno != errno.EPERM:
             raise
 
 def invert_dict(d):
