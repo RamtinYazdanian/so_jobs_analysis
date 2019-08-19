39ff853cbcc64ab033c77de40b9d8f45bb3081b4
Ramtin Yazdanian <ramtin.yazdanian@gmail.com>
2019-08-19 14:18:03 +0200

    string fix

--- utilities/text_utils.py
+++ utilities/text_utils.py
@@ -128,7 +128,7 @@ def tokenise_stem_punkt_and_stopword(text, punkt_to_remove=PUNKT, remove_periods
     text = text.lower()
 
     if (remove_code):
-        text = remove_all_code(text)
+        text = str(remove_all_code(text))
     # Removes HTML tags that are one character long. The whitespace is necessary to distinguish between an innocent <
     # (which is ' &lt; ') and a < that's part of an HTML tag. Same with next regex and &gt;.
     # This comes first because the second pattern would not understand these and would skip to the next &gt;.
