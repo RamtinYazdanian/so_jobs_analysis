add7726cb27f93a5deae0b31de7abae1c5791e9f
Ramtin Yazdanian <ramtin.yazdanian@gmail.com>
2019-08-20 14:48:07 +0200

    decoding the string

--- calculate_first_use_date_for_tags.py
+++ calculate_first_use_date_for_tags.py
@@ -30,7 +30,7 @@ def main():
 
     in_rdd = in_rdd.filter(lambda x: get_field(x[1], 'Tags') is not None and get_field(x[1], 'CreationDate') is not None).\
                     map(lambda x: (datetime.strptime(get_field(x[1], 'CreationDate').decode('utf-8'), DT_FORMAT),
-                                   get_tags(get_field(x[1], 'Tags')))).\
+                                   get_tags(get_field(x[1], 'Tags').decode('utf-8')))).\
                     flatMap(lambda x: [(x[0], y) for y in x[1]])
 
     tag_date_df = in_rdd.toDF(['CreationDate', 'Tag'])
