05e571f78230d5d14402b0449e80849dbdc9f882
Ramtin Yazdanian <ramtin.yazdanian@gmail.com>
2019-08-28 15:10:35 +0200

    converts the word2vec dataframe to a json and a pickle, the word dictionary and the vector matrix

---
+++ word2vec_to_matrix.py
@@ -0,0 +1,29 @@
+import os
+import argparse
+import pickle
+import numpy as np
+import json
+import py4j
+from utilities.common_utils import make_sure_path_exists
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--word2vec', type=str, required=True)
+    parser.add_argument('--output_dir', type=str, required=True)
+    args = parser.parse_args()
+
+    df = pickle.load(open(args.word2vec, 'rb'))
+    words_map = {v:k for k,v in enumerate(df['word'].values)}
+    vectors_matrix = np.array(df['vector'].apply(lambda x: np.array(x)).values)
+
+    make_sure_path_exists(args.output_dir)
+
+    with open(os.path.join(args.output_dir, 'words_map.json'), 'w') as f:
+        json.dump(words_map, f)
+
+    with open(os.path.join(args.output_dir, 'vectors_matrix.pkl'), 'wb') as f:
+        pickle.dump(vectors_matrix, f)
+
+if __name__ == '__main__':
+    main()
