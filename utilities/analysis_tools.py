a79c378a610489761025809bfcd76a8b545b0581
Ramtin Yazdanian <ramtin.yazdanian@gmail.com>
2019-11-11 10:35:47 +0100

    refactoring

--- utilities/analysis_tools.py
+++ utilities/analysis_tools.py
@@ -287,8 +287,8 @@ def get_all_proportions(df, seq_col, starting_elems):
 def calculate_ordering_proportion(df, seq_col, first_elem, second_elem):
     return (df.loc[df[seq_col].apply(lambda x: x.index(first_elem) != -1 and x.index(second_elem) != -1 and
         x.index(first_elem) < x.index(second_elem))].TagName.sum() / \
-        df.loc[df[seq_col].apply(lambda x: x.index(first_elem) != -1 and x.index(second_elem) != -1)].TagName.sum(),
-        df.loc[df[seq_col].apply(lambda x: x.index(first_elem) != -1 and x.index(second_elem) != -1)].TagName.sum())
+        df.loc[df[seq_col].apply(lambda x: x.index(first_elem) != -1 and x.index(second_elem) != -1)].shape[0],
+        df.loc[df[seq_col].apply(lambda x: x.index(first_elem) != -1 and x.index(second_elem) != -1)].shape[0])
 
 
 def get_topic_and_type(combined_tag_type):
